b45eaea86fca576f9851d488e36d7d33c77f6814
NileshPatil-18 <patilnilesharjun@gmail.com>
2026-01-20 08:24:59 +0530

    added frontend live url in backend cors

--- server/server.js
+++ server/server.js
@@ -29,7 +29,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://shopclues-web-clone-1.onrender.com"
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
